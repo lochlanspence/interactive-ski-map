737a531a47a2e73e5ce6faa1cd35d9f51d26b07c
19lspence <19lspence@wakatipu.school.nz>
2023-06-15 13:22:19 +1200

    Added the our features to the landing page and styled it

--- src/App.js
+++ src/App.js
@@ -2,28 +2,35 @@ import './App.css';
 import primary_logo_white from './images/NZSki_RM_Logo_01_Primary_White.png'
 import primary_logo_black from './images/Remarkables-BlackOut.png'
 import landing_image from './images/Fri13th-Snowshoot-15.jpg'
+import ice_bar from './images/ice-bar-wide-shot.jpeg'
 
 function App() {
   return (
-    <div className="App">
-      <div class="header">
+    <div classNameName="App">
+      <div className="header">
         <nav>
-          <ul class="navbar">
-            <li><a href="#"><img src={primary_logo_white} alt="Primary White Logo" class="primary_logo_white"></img></a></li>
-            <li><a href="#" class="nav_item">Our Mountain</a></li>
-            <li><a href="#" class="nav_item">Our Staff</a></li>
-            <li><a href="#" class="nav_item">Contact Us</a></li>
-            <li><a href="#" class="primary_button">View Map</a></li>
+          <ul className="navbar">
+            <li><a href="#"><img src={primary_logo_white} alt="Primary White Logo" className="primary_logo_white"></img></a></li>
+            <li><a href="#" className="nav_item">Our Mountain</a></li>
+            <li><a href="#" className="nav_item">Our Staff</a></li>
+            <li><a href="#" className="nav_item">Contact Us</a></li>
+            <li><a href="#" className="primary_button">View Map</a></li>
           </ul>
         </nav>
-        <div class="landing">
+        <div className="landing">
           <h1>The Remarkables</h1>
           <p>Queenstown's Adventure Playground</p>
         </div>
       </div>
-      <div class="mountain_features">
-          <h2>Our Mountain Features</h2>
-          <p>Placholder text about mountain features</p>
+      <div className="mountain_features sub-div">
+          <h2>Mountain Features</h2>
+          <div>
+          <img src={ice_bar} alt='Ice Bar'></img>
+          </div>
+          <div className='text'>
+            <p>Placholder text about mountain features</p>
+            <a href='#'>Learn More</a>
+          </div>
       </div>
     </div>
   );

1db1188310c3bd1e675e53bba93d7de012ed2249
lochlanspence <you@example.com>
2023-06-19 09:00:22 +1200

    Added some of the mountain features to the landing page with placeholder text

--- src/App.js
+++ src/App.js
@@ -3,6 +3,10 @@ import primary_logo_white from './images/NZSki_RM_Logo_01_Primary_White.png'
 import primary_logo_black from './images/Remarkables-BlackOut.png'
 import landing_image from './images/Fri13th-Snowshoot-15.jpg'
 import ice_bar from './images/ice-bar-wide-shot.jpeg'
+import sightseeing from './images/sightseeing.jpg'
+import tubing from './images/tubing.jpg'
+import stash from './images/Stash.jpg'
+import ice_bar_closeup from './images/ice-bar-closeup.jpg'
 
 function App() {
   return (
@@ -31,6 +35,31 @@ function App() {
             <p>Placholder text about mountain features</p>
             <a href='#'>Learn More</a>
           </div>
+      </div>
+      <div className='features_grid'>
+          <div className='stash'>
+            <h3>The Stash</h3>
+            <img src={stash} alt='Stash'></img>
+            <p>Placeholder Text</p>
+          </div>
+          <div className='icebar'>
+            <h3>Ice Bar</h3>
+            <img src={ice_bar_closeup} alt='Ice Bar'></img>
+            <p>Placeholder Text</p>
+          </div>
+          <div className='sightseeing'>
+            <h3>Sightseeing</h3>
+            <img src={sightseeing} alt='sightseeing'></img>
+            <p>Placeholder Text</p>
+          </div>
+          <div className='tubing'>
+            <h3>Tubing</h3>
+            <img src={tubing} alt='tubing'></img>
+            <p>Placeholder Text</p>
+          </div>
+      </div>
+      <div className='footer'>
+        <p>© The Remarkables 2023. All Rights Reserved</p>
       </div>
     </div>
   );

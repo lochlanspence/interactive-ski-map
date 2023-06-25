0548a96bf385a933562c9e9b3e983417426a21bd
19lspence <19lspence@wakatipu.school.nz>
2023-06-25 18:37:12 +1200

    Installed GH Pages Moved Broswer Router to correct place Replaced Browser Router with Hash Router

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
 import React from "react";
-import { Link, Route, Routes } from "react-router-dom";
+import { HashRouter, Route, Routes } from "react-router-dom";
 
 import Home from "../src/Pages/Home";
 import Courses from "./Pages/Our_mountain.js";
 import Our_mountain from "./Pages/Our_mountain.js";
 
+ {/* Defining routes path and rendering components as element */}
 function App() {
   return (
-    <div className="container">
-      {/* Defining routes path and rendering components as element */}
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/Our_mountain" element={<Our_mountain />} />
-      </Routes>
-    </div>
+
+      <HashRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/Our_mountain" element={<Our_mountain />} />
+        </Routes>
+      </HashRouter>
+    
   );
 }
 

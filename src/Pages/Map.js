7932317b3c8a8da06eadef445161015b6d6368dd
lochlanspence <you@example.com>
2023-07-20 09:31:58 +1200

    Added the ability to click the map the local browser displays the x and y values of the click position

--- src/Pages/Map.js
+++ src/Pages/Map.js
@@ -1,18 +1,22 @@
-import React from 'react'
-import Navbar from '../Components/Nav.js'
+import React, { useState } from 'react';
+import Navbar from '../Components/Nav.js';
 import styles from '../Map.module.css';
 import primary_logo_black from '../images/Remarkables-BlackOut.png';
 import TrailMap from '../images/Remarkables_Trail_Map.png';
 
 function Map() {
+  function handleMapClick(e) {
+    alert(`x ${e.clientX} y ${e.clientY}`);
+  }
+
   return (
-    <div className='Map'>
-      <Navbar logo={primary_logo_black}/>
-      <div className={styles.map_container}>
-        <img src={TrailMap}></img>
+    <div className="Map">
+      <Navbar logo={primary_logo_black} />
+      <div className={styles.map_container} onClick={handleMapClick}>
+        <img src={TrailMap} alt="Trail Map" />
       </div>
     </div>
-  )
+  );
 }
 
-export default Map
+export default Map;

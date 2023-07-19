0c1c41475734505958600861c22c43248fb9817f
lochlanspence <you@example.com>
2023-07-20 09:54:30 +1200

    calculates the x and y position of the map based on scale size

--- src/Pages/Map.js
+++ src/Pages/Map.js
@@ -2,14 +2,27 @@ import Navbar from '../Components/Nav.js';
 import styles from '../Map.module.css';
 import primary_logo_black from '../images/Remarkables-BlackOut.png';
 import TrailMap from '../images/Remarkables_Trail_Map.png';
+import {useRef} from 'react';
 
 function Map() {
+  const MapWidth = 1600;
+  const MapHeight = 900;
+  const mapRef = useRef(null);
   function handleMapClick(e) {
-    alert(`x ${e.clientX} y ${e.clientY}`);
+    const map = mapRef.current;
+
+    var rect = map.getBoundingClientRect();
+    var scaleX = map.width / rect.width;
+    var scaleY = map.height / rect.height;
+
+    var mouseX = Math.round(e.clientX - rect.left) * scaleX;
+    var mouseY = Math.round(e.clientY - rect.top) * scaleY;
+
+    alert(`x:${mouseX}, y:${mouseY}  mousex:${e.clientX}, mousey:${e.clientY}`);
   }
 
   return (
-    <div className="Map">
+    <div className="Map" ref={mapRef}>
       <Navbar logo={primary_logo_black} />
       <div className={styles.map_container} onClick={handleMapClick}>
         <img src={TrailMap} alt="Trail Map" />

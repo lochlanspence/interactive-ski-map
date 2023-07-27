be919e89990089e63cbac200016df98d87cf0b18
lochlanspence <you@example.com>
2023-07-28 10:52:19 +1200

    Added error prevention for when the user puts cursor to the top of the screen and causes an error in the code I did this by checking if the user is focused on the page or not

--- src/Pages/Map.js
+++ src/Pages/Map.js
@@ -2,51 +2,68 @@ import Navbar from '../Components/Nav.js';
 import styles from '../Map.module.css';
 import primary_logo_black from '../images/Remarkables-BlackOut.png';
 import TrailMap from '../images/Remarkables_Trail_Map.png';
-import {useRef, useState, useLayoutEffect, useCallback, useEffect} from 'react';
+import { useRef, useState, useLayoutEffect, useCallback, useEffect } from 'react';
 import Tile from '../Components/Tile.js';
 
 function Map() {
   // Dimensions of the map image used for calculating the scale for positioning tiles
   const MapWidth = 2048;
   const MapHeight = 1259;
-  
+
   // reference to the HTML element that contains the map image
   const mapRef = useRef(null);
-  
+
   // state management for the list of tiles to overlay on the map image
   const [tiles, setTiles] = useState([]);
-  
+
   // X/Y values to scale the tile positions based on the map size. 
   const scaleX = useRef(1.0);
   const scaleY = useRef(1.0);
-  const [, setWindowSize] = useState(0);
 
+  // Flag to indicate if the window is currently focused or not
+  const [isWindowFocused, setWindowFocused] = useState(true);
+
+  // Function to handle window focus change
+  const handleWindowFocusChange = useCallback(() => {
+    setWindowFocused(document.hasFocus());
+  }, []);
+
+  // Function to calculate the scale values based on the map container's size
   const setScale = useCallback(() => {
     const map = mapRef.current;
     const rect = map.getBoundingClientRect();
 
     scaleX.current = MapWidth / rect.width;
     scaleY.current = MapHeight / rect.height;
-  }, [scaleX, scaleY]);
+  }, [MapWidth, MapHeight]);
 
+  // Function to handle window resize
   const handleWindowResize = useCallback(() => {
     setScale();
-    window.addEventListener(window.innerWidth);
   }, [setScale]);
 
   useEffect(() => {
     setScale();
     window.addEventListener('resize', handleWindowResize);
+    window.addEventListener('focus', handleWindowFocusChange); // Listen for window focus change
+    window.addEventListener('blur', handleWindowFocusChange);  // Listen for window blur change
     return () => {
       window.removeEventListener('resize', handleWindowResize);
-    }
-  }, [setScale, handleWindowResize]);
+      window.removeEventListener('focus', handleWindowFocusChange);
+      window.removeEventListener('blur', handleWindowFocusChange);
+    };
+  }, [setScale, handleWindowResize, handleWindowFocusChange]);
 
   const getUniqueId = () => {
     return parseInt(Date.now())
   }
 
   function handleMapClick(e) {
+    // Check if the window is currently focused before proceeding
+    if (!isWindowFocused) {
+      return;
+    }
+
     const map = mapRef.current;
     const rect = map.getBoundingClientRect();
 
@@ -75,7 +92,7 @@ function Map() {
             const x = Math.round(t.x / scaleX.current);
             const y = Math.round(t.y / scaleY.current);
             return (
-              <Tile key={t.id} tile={t} x={x} y={y}/>
+              <Tile key={t.id} tile={t} x={x} y={y} />
             )
           })}
           <img src={TrailMap} alt="Trail Map" width="100%" />

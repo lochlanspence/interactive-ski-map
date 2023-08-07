848660888b242914bd13087ae55d72b70625f3b0
lochlanspence <you@example.com>
2023-08-07 12:50:44 +1200

    Added the ability to close the modal window

--- src/Components/Modal.js
+++ src/Components/Modal.js
@@ -1,13 +1,24 @@
-// In Modal.js
 import React from 'react';
 import styles from '../Modal.module.css';
 
 function Modal({ onClose, tile }) {
+  const handleDeleteClick = () => {
+    // Call the onClose function to close the modal
+    onClose();
+  };
+
   return (
     <div className={styles.modal}>
       <div className={styles['modal-content']}>
         {/* Use the tile data to display specific information */}
-        <h2>Modal Content</h2>
+        <h2>{tile.title}</h2>
+        <div className={styles.button_container}>
+          {/* Add onClick event handler for the "Delete" button */}
+          <p className={styles.delete_button} onClick={handleDeleteClick}>
+            Delete
+          </p>
+          <p className={styles.place_button}>Place</p>
+        </div>
       </div>
     </div>
   );

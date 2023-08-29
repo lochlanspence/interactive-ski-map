de2fb3a3f8e399e21f02166791365ec07e205c24
19lspence <19lspence@wakatipu.school.nz>
2023-08-29 16:51:17 +1200

    Added better hover effect for icons Moved the popup after place button is clicked to where the original coordinates are

--- src/Components/Tile.js
+++ src/Components/Tile.js
@@ -3,9 +3,16 @@ import styles from '../Map.module.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 function Tile({ tile, x, y }) {
+  // Adjust the positioning based on the tile's x and y values
+  const tileStyle = {
+    left: `${x}px`,
+    top: `${y}px`,
+    position: 'absolute',
+  };
+
   return (
-    <div className='tile' style={{ left: `${x}px`, top:`${y}px` }}>
-      {/*Renders the selected logo */}
+    <div className='tile' style={tileStyle}>
+      {/* Renders the selected logo */}
       {tile.logo && <FontAwesomeIcon icon={tile.logo} className={styles.logo} />}
       <span className={styles.tile_title}>{tile.title}</span>
     </div>

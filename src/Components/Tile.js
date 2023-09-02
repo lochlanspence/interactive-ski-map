c4729e7f3667d910e13ed7c0f92b7844931367ba
19lspence <19lspence@wakatipu.school.nz>
2023-09-03 10:07:13 +1200

    Added the ability to open the modal from edit widget button

--- src/Components/Tile.js
+++ src/Components/Tile.js
@@ -1,14 +1,20 @@
 import React, { useState } from 'react';
 import styles from '../Map.module.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import Modal from './Modal';
 
 function Tile({ tile, x, y }) {
   const [hovered, setHovered] = useState(false);
+  const [showModal, setShowModal] = useState(false); // State to control modal visibility
 
   const tileStyle = {
     left: `${x}px`,
     top: `${y}px`,
     position: 'absolute',
+  };
+
+  const toggleModal = () => {
+    setShowModal(!showModal);
   };
 
   return (
@@ -25,9 +31,14 @@ function Tile({ tile, x, y }) {
             <div className={styles.hoverContent}>
               <h3>{tile.title}</h3>
               <p>{tile.description}</p>
+              <button onClick={toggleModal}>Edit Widget</button> {/* Open the modal on button click */}
             </div>
           )}
         </div>
+      )}
+
+      {showModal && (
+        <Modal onClose={toggleModal} tile={tile} />
       )}
     </div>
   );
